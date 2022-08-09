c77b5e8eee598a8aa9f2223de1eccc4a86acb450
gosjgo <sjcoder9611@naver.com>
2022-08-09 22:44:02 +0900

    Sj commit please

--- BaekjoonPractice_PSJ/src/BJ_10158_개미.java
+++ BaekjoonPractice_PSJ/src/BJ_10158_개미.java
@@ -5,7 +5,8 @@ public class BJ_10158_°³¹Ì {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		int N = sc.nextInt();//sj
+		
 	}
 
 }
