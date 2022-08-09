a7a6d2b5cec65e9a3b376f831a26a7f66f667efc
gosjgo <sjcoder9611@naver.com>
2022-08-09 22:38:33 +0900

    SJ's first commit with eclipse's InterLocking

--- BaekjoonPractice_PSJ/src/BJ_10158_개미.java
+++ BaekjoonPractice_PSJ/src/BJ_10158_개미.java
@@ -5,6 +5,7 @@ public class BJ_10158_°³¹Ì {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
 	}
 
 }
